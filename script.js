97b0d6700b3580e8b77f124280e322e2a0e5b110
Ismayil Ismayilov <Psixo2015@mail.ru>
2023-09-17 13:35:43 +0400

    finish

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+
+//==============================Variable-CONST
+
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window')
+const modalWindow = document.querySelector('.modal-window')
+const overlay = document.querySelector('.overlay')
+const btnCloseModalWindow = document.querySelector('.close-modal-window')
+
+//==============================Function
+
+const closeModalWindow = () => {
+   modalWindow.classList.add('hidden')
+   overlay.classList.add('hidden')
+}
+
+const showModalWindow = () => {
+   modalWindow.classList.remove('hidden')
+   overlay.classList.remove('hidden')
+}
+
+//==============================Code
+
+//==============================Event
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+   btnsShowModalWindow[i].addEventListener('click', showModalWindow)
+}
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow)
+
+overlay.addEventListener('click', closeModalWindow)
+
+document.addEventListener('keydown', (e) => {
+   if (!modalWindow.classList.contains('hidden') && e.key === 'Escape') {
+      closeModalWindow()
+   }
+})
+
+
